1ac4363977ded1da957ef5b729611d741ac9fb96
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-11-14 06:35:53 +0530

    Final commits

--- weatherapi.py
+++ weatherapi.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 api_key = "1c4aaa597dd29ba9c44da6839c89fbf6"
 
-st.title("Weather Trakker☁️")
+st.title("Weather Tracker☁️")
 prompt = st.text_input("Which city do you want to know the weather of?")
 
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -16,12 +16,10 @@ if prompt:
 
     if response.status_code == 200:
         response_text = requests.get(complete_url).json()
-        print(type(response_text))
 
-    for i in response_text.keys():
-        if i == "coord":
-            st.write(response_text.get(i))
-        else:
-            continue
+        for i in response_text.keys():
+            if i == "main":
+                st.write(response_text.get(i))
+
     else:
         st.write("Please give a correct city name.")
