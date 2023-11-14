5a083b3d465b7594f967da8b0b39f12c6d255eec
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-11-14 06:13:09 +0530

    new file:   weatherapi.py

---
+++ weatherapi.py
@@ -0,0 +1,28 @@
+import os
+import requests
+import streamlit as st
+
+api_key = "1c4aaa597dd29ba9c44da6839c89fbf6"
+
+#App Framework
+st.title("Weather Trakker☁️")
+prompt = st.text_input("Which city do you want to know the weather of?")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+complete_url = base_url + "appid=" + api_key + "&q=" + prompt
+
+print(complete_url)
+if prompt:
+    response = requests.post(complete_url, json={"prompt": prompt})
+
+    if response.status_code == 200:
+        response_text = requests.get(complete_url).json()
+        print(type(response_text))
+
+    for i in response_text.keys():
+        if i == "coord":
+            st.write(response_text.get(i))
+        else:
+            continue
+    else:
+        st.write("Please give a correct city name.")
